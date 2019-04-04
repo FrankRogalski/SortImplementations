01ec3f7f4bed1a03351e9e7a7d203e08e49f4921
frankrogalski <frank.rogalski@hansa-flex.com>
2019-04-04 17:33:02 +0200

    Bugfixes

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("display.fxml"));
+        primaryStage.setTitle("Sorting algorithms");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
